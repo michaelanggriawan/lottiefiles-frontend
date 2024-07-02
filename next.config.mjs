678ce4ab4371a2513dfb11d00d9c0f518c96d206
michaelanggriawan <michaelanggriawan941@gmail.com>
2024-07-02 17:03:45 +0800

    support offline access

--- next.config.mjs
+++ next.config.mjs
@@ -1,62 +1,20 @@
-import withPWA from 'next-pwa';
+// import withPWA from 'next-pwa';
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,      // Enable React strict mode for improved error handling
-    swcMinify: true,            // Enable SWC minification for improved performance
-    compiler: {
-        removeConsole: process.env.NODE_ENV !== "development"     // Remove console.log in production
-    },
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     reactStrictMode: true,      // Enable React strict mode for improved error handling
+//     swcMinify: true,            // Enable SWC minification for improved performance
+//     compiler: {
+//         removeConsole: process.env.NODE_ENV !== "development"     // Remove console.log in production
+//     },
+// };
 
-export default withPWA({
-    dest: "public",         // destination directory for the PWA files
-    register: true,         // register the PWA service worker
-    buildExcludes: [/middleware-manifest.json$/],
-    runtimeCaching: [
-        {
-            urlPattern: /^https:\/\/lottiefiles-backend-production.up.railway.app\/graphql$/,
-            handler: 'NetworkFirst',
-            options: {
-                cacheName: 'api-cache',
-                networkTimeoutSeconds: 10,
-                expiration: {
-                    maxEntries: 50,
-                    maxAgeSeconds: 5 * 60, // 5 minutes
-                },
-                cacheableResponse: {
-                    statuses: [0, 200],
-                },
-            },
-        },
-        {
-            urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp)$/,
-            handler: 'CacheFirst',
-            options: {
-                cacheName: 'image-cache',
-                expiration: {
-                    maxEntries: 50,
-                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
-                },
-            },
-        },
-        {
-            urlPattern: /\.(?:js|css)$/,
-            handler: 'StaleWhileRevalidate',
-            options: {
-                cacheName: 'static-resources',
-            },
-        },
-        {
-            urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/,
-            handler: 'CacheFirst',
-            options: {
-                cacheName: 'google-fonts',
-                expiration: {
-                    maxEntries: 20,
-                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
-                },
-            },
-        },
-    ],
-})(nextConfig);
+// export default withPWA({
+//     dest: "public",         // destination directory for the PWA files
+//     register: true, 
+//     swSrc: 'service-worker.js'      // register the PWA service worker
+// })(nextConfig);
+
+export default {
+    reactStrictMode: true
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,26 @@
-import type { Metadata } from "next";
+"use client"
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ApolloWrapper } from "@/app/lib/apollo.wrapper";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "LottieFiles",
-  description: "It's a simple progressive web application made with NextJS",
-  generator: "Next.js",
-  manifest: "/manifest.json",
-  keywords: ["nextjs", "next14", "pwa", "next-pwa"],
-  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
-  authors: [
-    {
-      name: "Michael Anggriawan",
-      url: "https://www.linkedin.com/in/michael-anggriawan-a65707184",
-    },
-  ],
-  viewport:
-    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
-  icons: [
-    { rel: "apple-touch-icon", url: "ios/128.png" },
-    { rel: "icon", url: "ios/128.png" },
-  ],
-};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/service-worker.js', { scope: '/' })
+        .then((registration) => console.log('scope is: ', registration.scope));
+    }
+  }, []);
+  
   return (
     <html lang="en">
       <ApolloWrapper>
